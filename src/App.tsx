a24e4fd8accfcfab6bd24beeb51af19afa6e051e
root <root@FabioHauck>
2025-03-16 21:15:09 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import styled from 'styled-components'
+
+function App() {
+  return (
+   <></>
+  );
+}
+
+export default App;
